1b2d04c2ad194cec46774c22272cdd2247da7cd4
fs-s <joonas.puuding@ametikool.ee>
2022-06-04 22:36:05 +0300

    ´plz

--- deploy.php
+++ deploy.php
@@ -11,14 +11,14 @@ set('http_user', 'virt83023');
 set('keep_releases', 2);
 
 // Hosts
-host('tak19pyyding.itmajakas.ee')
+host('tak19ratsep.itmajakas.ee')
     ->setHostname('tak19pyyding.itmajakas.ee')
     ->set('http_user', 'virt83023')
-    ->set('deploy_path', '~/domeenid/tak19pyyding.itmajakas.ee/hajus')
-    ->set('branch', 'dev');
+    ->set('deploy_path', '~/domeenid/www.tak19pyyding.itmajakas.ee/hajus')
+    ->set('branch', 'main');
 
 // Tasks
-set('repository', 'git@github.com:fs-s/hajused.git');
+set('repository', 'git@github.com:fs-s/hajus.git');
 //Restart opcache
 task('opcache:clear', function () {
     run('killall php80-cgi || true');
